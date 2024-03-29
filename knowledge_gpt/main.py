e07c9400a083e87cac79ff0496892840785a9a81
wangn25 <45310885+wangn25@users.noreply.github.com>
2024-03-29 11:11:54 +0800

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -122,10 +122,10 @@ if submit:
             
 
 hide_st_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-    """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
 st.markdown(hide_st_style, unsafe_allow_html=True)
