7c637eac719a7e85e89614405e146ebddd68abdd
wangn25 <45310885+wangn25@users.noreply.github.com>
2024-03-29 10:36:30 +0800

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -122,14 +122,14 @@ if submit:
             
 footer="
 
-<style> your css code put here</style>
+    <style> your css code put here</style>
 
-<div class='footer'>
+    <div class='footer'>
 
-<p>the word you want to tell<a style='display:block;text-align:center;' 
+    <p>the word you want to tell<a style='display:block;text-align:center;' 
 
-href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
+    href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
 
-</div>"
+    </div>"
 
 st.markdown(footer, unsafe_allow_html=True)
